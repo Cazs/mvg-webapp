49fe778a57861063699f47bb4f74727e84504203
Cazs <casperndlovu42@gmail.com>
2017-04-16 14:01:41 +0200

    General UI updates - across all pages, working (mainly) on services page but currently incomplete. Introduced content for about us page.

--- frontend/controllers/services.js
+++ frontend/controllers/services.js
@@ -1,4 +1,11 @@
-mvg.controller('ServicesController',['$http','$location','$scope','$routeParams',function()
+//mvg.controller('ServicesController',['$http','$location','$scope','$routeParams',function($scope, $http, $location, $routeParams)
+mvg.controller('ServicesController',function($scope)
 {
-  console.log('Init ServicesController.');
-}]);
+  $scope.liveconcerts = [
+                        {'name':'live concert event a','date':0,'venue':'a venue','price':200},
+                        {'name':'live concert event b','date':1,'venue':'b venue','price':200},
+                        {'name':'live concert event c','date':2,'venue':'c venue','price':200},
+                        {'name':'live concert event c','date':2,'venue':'c venue','price':200},
+                        {'name':'live concert event c','date':2,'venue':'c venue','price':200}];
+    console.log('Init ServicesController.');
+});
