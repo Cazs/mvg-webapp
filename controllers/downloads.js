d50080a4f132c5b1ae2ed14ea35a1d8ed8f3894c
Casper Ndlovu <casperndlovu42@gmail.com>
2017-12-03 01:15:21 +0200

    Restructured webapp to conform to new MVG ecosystem. Merged webapp server with MVG-engine. Introduced Downloads page for downloading binary files relevant to MVG.

---
+++ controllers/downloads.js
@@ -0,0 +1,11 @@
+//mvg.controller('ServicesController',['$http','$location','$scope','$routeParams',function($scope, $http, $location, $routeParams)
+mvg.controller('DownloadsController', function($scope)
+{
+  $scope.liveconcerts = [
+                        {'name':'live concert event a','date':0,'venue':'a venue','price':200},
+                        {'name':'live concert event b','date':1,'venue':'b venue','price':200},
+                        {'name':'live concert event c','date':2,'venue':'c venue','price':200},
+                        {'name':'live concert event c','date':2,'venue':'c venue','price':200},
+                        {'name':'live concert event c','date':2,'venue':'c venue','price':200}];
+    console.log('Init DownloadsController.');
+});
--- models/system/access_levels.js
+++
@@ -1,7 +0,0 @@
-var AccessLevel = module.exports = new function()
-{
-  this.ALL = 0;
-  this.NORMAL = 1;
-  this.ADMIN = 2;
-  this.SUPER_USER = 3;
-}
